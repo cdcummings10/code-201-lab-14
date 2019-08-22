9fe8b24146b3ad41819b72a85ceef330c053dc71
cdcummings10 <ccummings0913@gmail.com>
2019-08-22 14:39:06 -0700

    Did some CSS, populated dropdown with items

--- starter-code/js/catalog.js
+++ starter-code/js/catalog.js
@@ -8,21 +8,21 @@ var cart = new Cart([]);
 // On screen load, we call this method to put all of the busmall options
 // (the things in the Product.allProducts array) into the drop down list.
 function populateForm() {
-
-  //TODO: Add an <option> tag inside the form's select for each product
+  //Add an <option> tag inside the form's select for each product
   var selectElement = document.getElementById('items');
-  for (var i in Product.allProducts) {
-
+  for (var i = 0; i < Product.allProducts.length; i++) {
+    var newOption = document.createElement('option');
+    newOption.textContent = Product.allProducts[i].name;
+    newOption.value = Product.allProducts[i].name;
+    selectElement.appendChild(newOption);
   }
-
 }
 
 // When someone submits the form, we need to add the selected item to the cart
 // object, save the whole thing back to local storage and update the screen
 // so that it shows the # of items in the cart and a quick preview of the cart itself.
-function handleSubmit(event) {
-
-  // TODO: Prevent the page from reloading
+function handleSubmit(e) {
+  e.preventDefault();
 
   // Do all the things ...
   addSelectedItemToCart();
@@ -35,6 +35,7 @@ function handleSubmit(event) {
 // TODO: Add the selected item and quantity to the cart
 function addSelectedItemToCart() {
   // TODO: suss out the item picked from the select list
+  
   // TODO: get the quantity
   // TODO: using those, add one item to the Cart
 }
